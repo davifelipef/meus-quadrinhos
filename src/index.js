db310fc597648c7089555cb01cd0e816ae4c6c5c
davifelipef <feliperedfield@gmail.com>
2022-05-28 14:25:52 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
 );
